dcb285fafa210c43cdd76739c13785208a056ff9
Juan Manjarres <juanmanjarres@jumanjas-macbook-pro.local>
2017-07-08 15:30:14 -0500

    Initial Commit

---
+++ miFormulario/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  miFormulario
+//
+//  Created by Juan Manjarres on 7/8/17.
+//  Copyright © 2017 jumanja.net. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
